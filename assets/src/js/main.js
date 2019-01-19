ac2b2d741a934f76680384d1fce299b2dad3ca97
Alexey Sachko <alexey.sachko@gmail.com>
2019-01-19 22:26:58 +0300

    Add popup window and js

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,1 +1,29 @@
-//= ./my.js
+document.addEventListener('DOMContentLoaded', () => {
+	let supportBtn 		 		= document.getElementById('supportBtn'),
+			popup			 		 		= document.getElementById('popup'),
+			popupBlock 		 		= document.getElementById('popupBlock'),
+			myBody		 		 		= document.body,
+			closeBtn	 		 		= document.getElementById('closeBtn'),
+			animationDuration = 500;
+
+	supportBtn.addEventListener('click', (event) => {
+		event.preventDefault();
+		if ( !myBody.classList.contains('noscroll') ) {
+			myBody.classList.add('noscroll');
+			popup.classList.remove('hidden');
+			popup.classList.add('visible');
+			setTimeout(function() {
+				popupBlock.classList.add('active');
+			}, 1);
+		}
+	});
+
+	closeBtn.addEventListener('click', () => {
+		myBody.classList.remove('noscroll');
+		popup.classList.remove('visible');
+		setTimeout(function() {
+			popup.classList.add('hidden');
+		}, animationDuration);
+		popupBlock.classList.remove('active');
+	});
+});
